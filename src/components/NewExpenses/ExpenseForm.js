d04bb35313247649a826ab1b6b661c536b1ab70f
kunal-gitt <kunalthakur2023@gmail.com>
2023-03-21 13:29:43 +0530

    Deliverables add state inside own component

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Expenseform.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  const [enteredtitle, setenteredtitle] = useState("");
+
+  const [enteredamount, setenteredamount] = useState("");
+  const [entereddate, setentereddate] = useState("");
+
   const titlechangehandler = (event) => {
-    console.log(event.target.value);
+    setenteredtitle(event.target.value);
   };
+
+  const amountchangehandler = (event) => {
+    setenteredamount(event.target.value);
+  };
+
+  const datechangehandler = (event) => {
+    setentereddate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -16,12 +30,22 @@ const ExpenseForm = () => {
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountchangehandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2023-12-31"
+            onChange={datechangehandler}
+          />
         </div>
       </div>
 
