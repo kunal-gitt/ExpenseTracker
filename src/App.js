7f3bac6b66c38bc3023fdb6ac86490e4102ab737
kunal-gitt <kunalthakur2023@gmail.com>
2023-03-23 17:09:11 +0530

    changed data to dollar

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ const DUMMY_EXPENSES = [
   {
     id: "e1",
     title: "Toilet paper",
-    amount: 70.7,
+    amount: "$70.7",
     date: new Date(2022, 3, 5),
   },
   {
     id: "e2",
     title: "Car insurance",
-    amount: 23.7,
+    amount: "$23.7",
     date: new Date(2021, 4, 5),
   },
   {
     id: "e3",
     title: "New TV",
-    amount: 155.7,
+    amount: "$155.7",
     date: new Date(2021, 3, 10),
   },
   {
     id: "e4",
     title: "Movie",
-    amount: 255.7,
+    amount: "$255.7",
     date: new Date(2021, 8, 5),
   },
 ];
