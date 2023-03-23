766b12d7f96ac7589bcfcfd472323caec94127c9
kunal-gitt <kunalthakur2023@gmail.com>
2023-03-23 17:08:46 +0530

    small change

--- src/components/Expenses/ExpensesItem.js
+++ src/components/Expenses/ExpensesItem.js
@@ -6,13 +6,15 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <ExpenseDetails
-        title={props.title}
-        amount={props.amount}
-      ></ExpenseDetails>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date}></ExpenseDate>
+        <ExpenseDetails
+          title={props.title}
+          amount={props.amount}
+        ></ExpenseDetails>
+      </Card>
+    </li>
   );
 };
 
